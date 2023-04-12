625f34c3911444d4e0fdb6ec3dce77ca6f4f1335
Homero Raul <h.raulvc@gmail.com>
2023-04-12 14:12:21 -0600

    Add GetAuth request

---
+++ src/Http/Requests/Auth/GetAuthRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Innoboxrr\LaravelAuth\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class GetAuthRequest extends FormRequest
+{
+
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [];
+    }
+
+    public function handle()
+    {
+
+        return $this->getResponse();
+
+    }
+
+    public function getResponse()
+    {
+
+        if ($this->wantsJson()) {
+
+            return response()->json([
+                'user' => $this->user()
+            ]);
+
+        } else {
+
+            return redirect(config('laravel-auth.routes.redirects.get-auth'))
+                ->with('user', $this->user());
+
+        }
+    
+    }
+
+}
