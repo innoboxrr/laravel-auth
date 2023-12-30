9f2c2aa72b045ef85c100c34936844038a09dcb8
Homero Raul <h.raulvc@gmail.com>
2023-12-30 00:04:02 -0600

    Fix

--- src/Http/Requests/Impersonate/ImpersonateRequest.php
+++ src/Http/Requests/Impersonate/ImpersonateRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Innoboxrr\LaravelAuth\Http\Requests\Auth;
+namespace Innoboxrr\LaravelAuth\Http\Requests\Impersonate;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
